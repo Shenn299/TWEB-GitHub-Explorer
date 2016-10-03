db8d208a8e1ed4a1422cbf4af0d7234352241d45
Shenn299 <sebastien.henneberger@gmail.com>
2016-10-03 17:30:09 +0200

    nice grap !h

---
+++ app/app.js
@@ -0,0 +1,28 @@
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc index
+	 * @name app
+	 * @description
+	 * # app
+	 *
+	 * Main modules of the application.
+	 */
+
+	angular.module('github-explorer', [
+		'ngResource',
+		'ngAria',
+		'ui.bootstrap',
+		'ngMaterial',
+		'ngMdIcons',
+		'ngCookies',
+		'ngAnimate',,
+		'ngSanitize',
+		'ui.router',
+		'page1',
+		'page2',
+		'page3',
+	]);
+
+})();
